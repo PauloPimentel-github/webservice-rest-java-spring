4d1e5879901bccf88aa8c22465af1806d2a94627
PauloPimentel <paulo.h.g.pimentel@gmail.com>
2018-08-20 16:37:52 -0300

    Atualizando configuração de segurança no token de acesso

--- src/main/java/com/devpimentel/restapi/restapi/config/AuthorizationServerConfig.java
+++ src/main/java/com/devpimentel/restapi/restapi/config/AuthorizationServerConfig.java
@@ -6,6 +6,7 @@ import com.devpimentel.restapi.restapi.config.token.CustomTokenEnhancer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -17,6 +18,7 @@ import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+@Profile("oauth-security")
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
